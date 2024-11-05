d38082788611d239f3a4ff61d6fcab6d2a032dc4
Marek Mościchowski <marek.moscichowski@miquido.com>
2024-11-05 07:50:08 +0100

    Initial Commit

---
+++ examples/complete/main.tf
@@ -0,0 +1,9 @@
+module "cicd-tf-dev" {
+  providers = {
+    aws = aws.dev
+  }
+  source                       = "../../"
+  environment                  = "dev"
+  gitlab_tf_admin_oidc_subs    = ["project_path:test/repo:ref_type:branch:ref:dev"]
+  gitlab_tf_readonly_oidc_subs = ["project_path:test/repo:ref_type:branch:ref:*"]
+}
