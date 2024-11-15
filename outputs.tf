3966f2658d2b195de9af8543ea1d0946ebb0b4fb
Marek Mościchowski <marek.moscichowski@miquido.com>
2024-11-15 08:53:42 +0100

    feat: added role name to output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "tf_admin_role_arn" {
 output "tf_readonly_role_arn" {
   value = aws_iam_role.terraform-readonly.arn
 }
+
+output "tf_admin_role_name" {
+  value = aws_iam_role.terraform.name
+}
+
+output "tf_readonly_role_name" {
+  value = aws_iam_role.terraform-readonly.name
+}
